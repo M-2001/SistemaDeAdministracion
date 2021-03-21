aca16efb0ad26615ccae9f2fc0883f8eeab8b0d3
M-2001 <59231436+M-2001@users.noreply.github.com>
2021-03-20 20:12:50 -0600

    Proteccion Rutas F

--- src/router/rating.ts
+++ src/router/rating.ts
@@ -2,19 +2,20 @@ import { Router } from "express";
 import { checkRole } from '../middleware/role';
 import { CheckJwt } from '../middleware/jwt';
 import RatingController from '../controller/Rating';
+import { checkRoleU } from '../middleware/roleUser';
 
 const router = Router();
 const rating = RatingController;
 
-router.post('/', [CheckJwt], rating.AgregarRating);
-router.get('/', rating.MostrarRating);
-router.get('/:id', rating.RatingPorId);
-router.put('/:id',[CheckJwt], rating.ActualizarRating);
-router.delete('/:id',[CheckJwt], rating.EliminarRating);
+router.post('/', [CheckJwt, checkRoleU(['user'])], rating.AgregarRating);
+router.get('/', [CheckJwt, checkRoleU(['user'])], rating.MostrarRating);
+router.get('/:id',[CheckJwt, checkRoleU(['user'])], rating.RatingPorId);
+router.put('/:id',[CheckJwt, checkRoleU(['user'])], rating.ActualizarRating);
+router.delete('/:id',[CheckJwt, checkRoleU(['user'])], rating.EliminarRating);
 //rating paginados
-router.post('/rating-paginated', rating.MostrarRatingPaginados);
+router.post('/rating-paginated',[CheckJwt, checkRoleU(['user'])], rating.MostrarRatingPaginados);
 
 //rating por producto
-router.post('/product', rating.MostrarRatingPorProducto);
+router.post('/product', [CheckJwt, checkRoleU(['user'])], rating.MostrarRatingPorProducto);
 
 export default router;
