949df0377c8e444ff7ee6651d3057f8c5fd528f7
Carlos Daniel Contreras <tiburonchan3@gmail.com>
2021-05-11 16:04:39 -0600

    fuck typescript and heroku

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import * as express from 'express';
 import Server from './server/index';
-import  * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 createConnection().then(async () => {
-
+    const app = express()
     dotenv.config()
-
+    app.listen(process.env.PORT, () => {
+        console.log("ready in port: ", process.env.PORT)
+    })
     const server = new Server();
     server.listen();
 
