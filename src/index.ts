38284835cb52c8aadb3a81774408a1b4f22c1356
Carlos Daniel Contreras <tiburonchan3@gmail.com>
2021-05-11 16:10:42 -0600

    aaaaaaaaaaaaaaaaa

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import * as express from 'express';
 import Server from './server/index';
 import * as dotenv from 'dotenv';
 
+dotenv.config()
 createConnection().then(async () => {
+    
     const app = express()
-    dotenv.config()
-    app.listen(process.env.PORT, () => {
-        console.log("ready in port: ", process.env.PORT)
-    })
+    var port_number = app.listen(process.env.PORT || 3000);
+    app.listen(port_number)
+    // app.listen(process.env.PORT, () => {
+    //     console.log("ready in port: ", process.env.PORT)
+    // })
     const server = new Server();
-    server.listen();
+    // server.listen();
 
 
 }).catch(error => console.log(error));
