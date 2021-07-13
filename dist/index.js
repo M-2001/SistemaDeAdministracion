55d59042dc10cb005c7d9761cb0febec914b96b5
M-2001 <59231436+M-2001@users.noreply.github.com>
2021-07-12 23:11:58 -0600

    ...

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 const SocketServer = index_1.default.instance;
 SocketServer.start(() => {
-    app.use(cors({ origin: ['https://client-systempc.vercel.app', "https://system-pc.netlify.app"], credentials: true }));
+    app.use(cors({ origin: ['https://client-systempc.vercel.app', "https://system-pc.netlify.app", 'https://admin-system-pc.vercel.app/'], credentials: true }));
     console.log('===> Servidor corriendo en puerto: ' + SocketServer.port);
     typeorm_1.createConnection().then(async (connection) => {
         if (connection) {
